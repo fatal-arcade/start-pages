1423d99adc4a852dbc10b5966a09e71eb53523ed
FATAL ARCADE <fatal_arcade@proton.me>
2026-07-23 07:08:09 -0700

    upload config file for search engines

--- scripts/widgets/search.js
+++ scripts/widgets/search.js
@@ -1,12 +1,52 @@
+function searchEngineDropdownItem(key, item) {
+    
+    data = item.json()
+
+    return `
+        <div class="engine-dropdown-item"
+            data-value=${key}
+            data-url=${''}
+            data-param=${''}
+            data-icon=${''}>
+            <img src=${''} class="engine-favicon">
+            ${item.id}
+        </div>
+    `
+}
+
+async function searchEngineDropdownList() {
+
+    try {
+
+        const resp = await fetch('../config/engines.json')
+
+        if (!resp.ok) {
+            throw new Error(`${resp.status}`)
+        }
+
+        const data = await resp.json();
+
+        const items = data.search_engines;
+
+        return items
+        
+    } catch (error) {
+
+        console.error('Failed to load search engines:', error);
+    }
+
+
+}
+
 export function Web(element) {
 
     const placeholder = element.dataset.placeholder || "Search the web...";
 
     element.innerHTML = `
         
-        <form id="searchForm" class="search-form" action="https://duckduckgo.com/" method="get" target="_blank">
+        <form id="search" class="search-bar" action="https://startpage.com/sp/search" method="get" target="_blank">
         
-            <input type="text" id="searchInput" name="q" class="search-input" placeholder="Search the web..." required autocomplete="off" autofocus>
+            <input type="text" id="searchInput" name="query" class="search-input" placeholder="Search" autocomplete="off" autofocus required>
 
         </form>
     `;
